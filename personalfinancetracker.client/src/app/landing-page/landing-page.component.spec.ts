594759efb1c737a9c15e7b33ed075ea0daa3ceba
Brandon Benefield <bsquared18@gmail.com>
2024-03-24 05:56:17 -0400

    Write unit tests for LandingPage

--- personalfinancetracker.client/src/app/landing-page/landing-page.component.spec.ts
+++ personalfinancetracker.client/src/app/landing-page/landing-page.component.spec.ts
@@ -3,21 +3,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingPageComponent } from './landing-page.component';
 
 describe('LandingPageComponent', () => {
-  let component: LandingPageComponent;
-  let fixture: ComponentFixture<LandingPageComponent>;
+    let component: LandingPageComponent;
+    let fixture: ComponentFixture<LandingPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LandingPageComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LandingPageComponent]
+        })
+        .compileComponents();
     
-    fixture = TestBed.createComponent(LandingPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(LandingPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have correct href for sign up link', () => {
+        const linkElements: HTMLElement[] = fixture.nativeElement.querySelectorAll('a');
+        const logoLink = Array.from(linkElements).find(element => element.textContent?.includes('Sign Up For Free'));
+        expect(logoLink).not.toBeNull()
+        expect(logoLink?.getAttribute("href")).toEqual("/signup")
+    });
 });
