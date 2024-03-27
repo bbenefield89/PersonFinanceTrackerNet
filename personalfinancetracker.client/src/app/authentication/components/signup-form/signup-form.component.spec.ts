c09ffc843e431bbfbc528484efa4da1e3a48fcc5
Brandon Benefield <bsquared18@gmail.com>
2024-03-27 06:18:47 -0400

    Fix broken tests

--- personalfinancetracker.client/src/app/authentication/components/signup-form/signup-form.component.spec.ts
+++ personalfinancetracker.client/src/app/authentication/components/signup-form/signup-form.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from "@angular/common/http/testing"
 import { SignupFormComponent } from './signup-form.component';
+import { UserRegistrationService } from '../../services/user-registration/user-registration.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 describe('SignupFormComponent', () => {
-  let component: SignupFormComponent;
-  let fixture: ComponentFixture<SignupFormComponent>;
+    let component: SignupFormComponent;
+    let fixture: ComponentFixture<SignupFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignupFormComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SignupFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SignupFormComponent],
+            imports: [
+                HttpClientTestingModule,
+                ReactiveFormsModule,
+            ],
+            providers: [
+                FormBuilder,
+                UserRegistrationService,
+            ],
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(SignupFormComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- personalfinancetracker.client/src/app/views/signup-login-page/signup-login-page.component.spec.ts
+++ personalfinancetracker.client/src/app/views/signup-login-page/signup-login-page.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupLoginPageComponent } from './signup-login-page.component';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-signup-form",
+    template: "<div></div>",
+})
+class MockSignupFormComponent { }
 
 describe('SignupLoginPageComponent', () => {
-  let component: SignupLoginPageComponent;
-  let fixture: ComponentFixture<SignupLoginPageComponent>;
+    let component: SignupLoginPageComponent;
+    let fixture: ComponentFixture<SignupLoginPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SignupLoginPageComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(SignupLoginPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                SignupLoginPageComponent,
+                MockSignupFormComponent,
+            ]
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(SignupLoginPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
